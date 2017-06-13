92d8a8fbe6b2930600ce492ca78a191b5feb2b81
Arbi Velaj <arbi.velaj@gmail.com>
2017-06-13 16:27:21 +0200

    user model - arbi

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :fields
+
+  get "user", to: "users#show"
+  #patch "users/:id", to: "users#update"
+  #put "users/:id", to: "users#update"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

2694b3e95b5862e7046346c1807d601da3524016
Rhea Akiki <rhea.akiki@gmail.com>
2017-06-13 14:56:35 +0200

    homepage again

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :fields, only: [:index, :show, :create, :new, :update, :destroy]
+  get '/search', to: 'fields#index', as: 'search'
 end

e73defaa71b2b976aeb4be01bf8082eb528d4a80
Rheaakiki <rhea.akiki@gmail.com>
2017-06-13 20:50:37 +0200

    clean routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   resources :fields
 
   get "user", to: "users#show"
-  #patch "users/:id", to: "users#update"
-  #put "users/:id", to: "users#update"
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

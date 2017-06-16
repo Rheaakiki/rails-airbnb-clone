63f56cd105168acac5eb9fccffc031056c3ce793
Rhea Akiki <rhea.akiki@gmail.com>
2017-06-16 15:23:05 +0200

    signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:phone_number, :bank_details, :username, :biography])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:phone_number, :username, :biography, :email, :password])
   end
 end
